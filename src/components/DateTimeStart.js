fec57196794cc04c40f24e55d34c6a4929e848f2
lancechincodes <lanceechin@gmail.com>
2022-07-10 01:34:03 -0500

    Add rotating clothes selection in weather info start component

---
+++ src/components/DateTimeStart.js
@@ -0,0 +1,12 @@
+import '../cssFiles/DateTime.css'
+
+function DateTimeStart() {
+    return(
+        <div className="dateTime">
+            <p className="time">12:00</p>
+            <p className="date">Sunday, July 10</p>
+        </div>
+    )
+}
+
+export default DateTimeStart
--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,10 +1,14 @@
 // import '../cssFiles/SearchPage.css'
+import DateTimeStart from './DateTimeStart'
 import SearchForm from './SearchForm'
+import WeatherInfoStart from './WeatherInfoStart'
 
 function SearchPage() {
     return (
         <div className="search-page">
+            <DateTimeStart />
             <SearchForm />
+            <WeatherInfoStart />
         </div>
     )
 }
